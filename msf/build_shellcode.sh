5255bd82a56f679184f626df4bd6b3737ab3e587
ClementB-etu <belleil.clement00@gmail.com>
2023-01-13 17:37:37 +0100

    commit (asm64 shellcode OK)

--- msf/build_shellcode.sh
+++ msf/build_shellcode.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
 
 nasm -f elf64 -o $2.bin ../$1/$2.s
-/opt/metasploit/app/msfvenom -e x64/shikata_ga_nai -f c -a x64 --platform linux -o $2_x64.shellcode < $2.bin
+/opt/metasploit/app/msfvenom -e x64/xor -f c -a x64 --platform linux -o $2_x64.shellcode < $2.bin
 rm $2.bin
