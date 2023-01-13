0442a55d418dd290ed492c3ed5ff0291c66fa734
ClementB-etu <belleil.clement00@gmail.com>
2023-01-13 10:50:38 +0100

    commit (create_shellcode OK)

--- asm32/create_shellcode.sh
+++ asm32/create_shellcode.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
-for i in $(objdump -d shell.o -M intel |grep "^ " |cut -f2); do echo -n '\x'$i; done;echo
+objdump -d $1 | grep -Po '\s\K[a-f0-9]{2}(?=\s)' | sed 's/^/\\x/g' | perl -pe 's/\r?\n//' | sed 's/$/\n/'
+#1) grep : récupères les bytes et les affiche en colonne (50 68 ...)
+#2) sed : ajoute \x avant les bytes
+#3) perl : affiche le résultat en ligne
+#4) sec : ajoute un retour à la ligne pour affichage
